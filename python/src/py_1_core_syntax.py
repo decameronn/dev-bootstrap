f485df8d81448e05294f853ded915f5e9ea57505
Decameron <czorile@gmail.com>
2025-04-06 19:59:07 +0300

    initial commit (python core syntax) (#1)

---
+++ python/src/py_1_core_syntax.py
@@ -0,0 +1,47 @@
+
+# Core Syntax & Data Types
+# ********************************************************
+# integer
+a = 10
+# float
+b = 10.5
+# string
+word = "Hello, World!"
+# boolean
+is_great = True
+
+# list ( ordered mutable collection )
+fruits = ["apple", "banana", "cherry"]
+
+# tuple ( ordered immutable collection )
+base_location = (43.12, 76.34)
+
+# set ( unordered collection of unique elements )
+odd_numbers = {1, 3, 5, 7, 9}
+
+# dictionary ( key-value pairs )
+person = {
+    "name": "John Doe",
+    "age": 30,
+    "is_student": False
+}
+
+# None ( represents the absence of a value )
+nothing = None
+
+
+# Practice
+# ********************************************************
+
+# 1. Create a list of three of your favorite movies.
+fav_movies = ["inception", "matrix", "stalker"]
+
+# 2. Create a dictionary to store your favorite movie along with its release year.
+fav_movie = { "inception": 2010 }
+
+# 3. Create a tuple for the coordinates of your favorite place.
+fav_place  = (40.7128, 74.0060) 
+
+
+
+
