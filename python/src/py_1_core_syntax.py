129d3f709bb1e555ab031fe7e6d952b768f8d987
Decameron <czorile@gmail.com>
2025-04-07 18:54:26 +0300

    accessing with index

--- python/src/py_1_core_syntax.py
+++ python/src/py_1_core_syntax.py
@@ -60,3 +60,12 @@ movie_rating = {
     "pandorum": 6.7
 }
 
+# 5. Access the 2nd movie from list_of_movies
+second_movie = list_of_movies[1]
+print(second_movie)
+
+# 6. Access the rating of "stalker" from movie_rating
+rating_stalker = movie_rating["matrix"]
+print(rating_stalker)
+
+
